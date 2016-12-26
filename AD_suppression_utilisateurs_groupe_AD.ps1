0516a0dd4e61e5f19b5eac69c380198295bb4d2e
SKirbiller <skirbiller@gmail.com>
2016-12-26 20:08:03 +0100

    AD - Suppression utilisateurs groupe AD

---
+++ AD_suppression_utilisateurs_groupe_AD.ps1
@@ -0,0 +1,41 @@
+﻿$group = "groupe AD"
+$fileUser = Get-Content c:\_admin\_script\AD\liste_remove_ad_group.txt
+$infoGroup = Get-ADGroup -Identity $group
+
+
+$i = 0
+
+
+foreach ($item in $fileUser)
+{
+	$InfoUser = Get-ADuser -Identity $item
+	$getGroupAdUser = Get-ADPrincipalGroupMembership $item | select name
+	foreach ($groupname in $getGroupAdUser)
+	{
+		if ($groupname -match "$group")
+		{
+			$Appartient = $true
+		}
+	}
+	
+	if ($Appartient -eq $true)
+	{
+		Try
+		{
+			Write-Host "Supression du groupe $group user : $InfoUser"
+			remove-adgroupmember -Identity $infoGroup.DistinguishedName -Member $InfoUser.DistinguishedName  -Confirm:$false
+			Write-Host "Supression du groupe $group : OK"
+		}
+		Catch
+		{
+			Write-Host "Supression du groupe $group : ERROR"
+		}
+	}
+	else
+	{
+		Write-Host "L'utilisateur $item ne fait pas parti du groupe : $group "
+	}
+	Write-Host ""
+}
+
+Write-Host "FIN"
