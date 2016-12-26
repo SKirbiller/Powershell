a7714eabda2fad67a1316e84d1df1b3cf5ac234b
Kirbillou <skirbiller@gmail.com>
2016-12-26 19:51:45 +0100

    AD - Export utilisateur de plusieurs groupes AD

---
+++ AD_export_des_utilisateurs_de_plusieurs_groupes_AD.ps1
@@ -0,0 +1,25 @@
+﻿$groupfile = Get-Content c:\_admin\_script\AD\liste_groupe_PCT.txt
+
+foreach ($Adgroup in $groupfile)
+{
+	$group = get-adgroupmember $Adgroup
+	$i = 0
+	
+	foreach ($item in $group)
+	{
+		$user = $item.name
+		
+		Try
+		{
+			$info = Get-ADuser -Identity $item -Properties GivenName
+			Write-Host $Adgroup ","  $user "," $info.givenname "," $info.surname
+			$i = $i+1
+		}
+		Catch
+		{
+			Write-Host $user
+		}
+		
+	}
+}
+Write-Host $i
