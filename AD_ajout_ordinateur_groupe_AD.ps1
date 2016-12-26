33a46d07a3fc411233bf1e6c53d3aaa09b173cd2
Kirbillou <skirbiller@gmail.com>
2016-12-26 19:50:15 +0100

    AD - Ajout ordinateur dans un groupe AD

---
+++ AD_ajout_ordinateur_groupe_AD.ps1
@@ -0,0 +1,35 @@
+﻿$computer_file = Get-Content c:\_admin\_script\AD\liste_computer_ad.txt
+$LogFile = "c:\_admin\_Script\AD\add_user_to_group_log.txt"
+
+function add_group_function
+{
+	PARAM(
+         [String]$GroupSite,
+         [String]$itemF
+       	 )
+		 
+	#Write-Host "$item en cours - $GroupSite"
+	#Write-Host $GroupSite
+	#Write-Host $itemF
+	Write-Host "Add-ADGroupMember -id $GroupSite -Members $itemF"
+	try 
+	{
+		Add-ADGroupMember -id $GroupSite -Members $itemF
+		$MyLine = "$item ajouté avec succès à $GroupSite"
+		add-content $LogFile $MyLine
+	}
+	catch
+	{
+		$MyLine = "$itemF ERREUR"
+		add-content $LogFile $MyLine			
+	}
+}
+
+foreach ($item in $computer_file)
+{
+	$pc = Get-ADComputer $item
+	$pcDN = $pc.DistinguishedName
+	
+	add_group_function -GroupSite "Groupe-SoftwareXXX" -itemF $pcDN
+
+}
